c610dab1204ed41be4b228d8394bdd7a7b65ef97
Kevin Ison <KevinIsonLDS@gmail.com>
2018-02-07 10:45:24 -0500

    innitial commit

---
+++ KiTimer/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace KiTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtDescription = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.LblMessage = new System.Windows.Forms.Label();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Description:";
+            // 
+            // TxtDescription
+            // 
+            this.TxtDescription.Location = new System.Drawing.Point(81, 12);
+            this.TxtDescription.Name = "TxtDescription";
+            this.TxtDescription.Size = new System.Drawing.Size(345, 20);
+            this.TxtDescription.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(100, 49);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(67, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Timer Minutues:";
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(130, 114);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(75, 23);
+            this.BtnStart.TabIndex = 4;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(234, 114);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(75, 23);
+            this.BtnExit.TabIndex = 5;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            // 
+            // LblMessage
+            // 
+            this.LblMessage.AutoSize = true;
+            this.LblMessage.Location = new System.Drawing.Point(12, 82);
+            this.LblMessage.Name = "LblMessage";
+            this.LblMessage.Size = new System.Drawing.Size(35, 13);
+            this.LblMessage.TabIndex = 6;
+            this.LblMessage.Text = "label3";
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Location = new System.Drawing.Point(191, 51);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(35, 13);
+            this.lblStart.TabIndex = 7;
+            this.lblStart.Text = "label3";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 149);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.LblMessage);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.TxtDescription);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My Timer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtDescription;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Label LblMessage;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ KiTimer/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+
+namespace KiTimer
+{
+    public partial class Form1 : Form
+    {
+        private Timer _timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Text = $@"My Tmer v{Application.ProductVersion}";
+
+            LblMessage.Text = "";
+            lblStart.Text = "";
+
+            _timer = new Timer();
+            _timer.Elapsed += _timer_Elapsed;
+
+            BtnExit.Click += (sender, args) => Close();
+            BtnStart.Click += BtnStart_Click;
+        }
+
+        public sealed override string Text
+        {
+            get => base.Text;
+            set => base.Text = value;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (BtnStart.Text.Contains("Start"))
+            {
+                var secs = (numericUpDown1.Value * 1000) * 60;
+                _timer.Interval = Convert.ToDouble(secs);
+                _timer.Enabled = true;
+                BtnStart.Text = @"Stop";
+                lblStart.Text = $@"started: {DateTime.Now.ToShortTimeString()}";
+                LblMessage.Text = @"...";
+                WindowState = FormWindowState.Minimized;
+                return;
+            }
+
+            // btn is now a stop button so...
+            _timer.Enabled = false;
+            BtnStart.Text = @"Start";
+        }
+
+        private void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate
+            {
+                LblMessage.ForeColor = Color.Brown;
+                LblMessage.Text = $@"{TxtDescription.Text}: TIME IS UP!";
+                BtnStart.Text = @"Start";
+                SendNotification();
+            }));
+        }
+
+        private void SendNotification()
+        {
+            notifyIcon1.Icon = SystemIcons.Application;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipTitle = $@"Time ALERT!!!";
+            notifyIcon1.BalloonTipText = $@"{TxtDescription.Text}!";
+            notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(15000);
+            WindowState = FormWindowState.Normal;
+        }
+    }
+}
